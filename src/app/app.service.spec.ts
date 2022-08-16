6ef6a464f7fab4dd6869010605e9f7b403bbe1ad
Ruan Almeida da Silva <ruanrodrigues1994@gmail.com>
2022-08-16 14:49:51 -0300

    Update app.service.spec.ts
    Ajustado o teste do itau

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { AppModule } from './app.module';
 
 import { AppService } from './app.service';
 
 describe('AppService', () => {
-  // let service: AppService;
+  let service: AppService;
 
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({});
-  //   service = TestBed.inject(AppService);
-  // });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        AppModule
+      ]
+    }).compileComponents();
+    service = TestBed.inject(AppService);
+  });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
